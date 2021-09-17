97ba0831f218b769819d52868dff4462eb803691
AndrewShchetkin <1g32MfsDb4>
2021-09-17 19:06:59 +0500

    chore

--- ClientApp/src/components/map/map.tsx
+++ ClientApp/src/components/map/map.tsx
@@ -3,36 +3,51 @@ import Unity, { UnityContext } from 'react-unity-webgl';
 
 
 interface GameState{
-    progress: number
+    progress: number,
+    coordinates: {x: number, y: number}
+
 }
 
 const unityContext = new UnityContext({
-    loaderUrl: "webGL/Build/build.loader.js",
-    dataUrl: "webGL/Build/build.data",
-    frameworkUrl: "webGL/Build/build.framework.js",
-    codeUrl: "webGL/Build/build.wasm",
+    loaderUrl: "webGL/Build/webGL.loader.js",
+    dataUrl: "webGL/Build/webGL.data",
+    frameworkUrl: "webGL/Build/webGL.framework.js",
+    codeUrl: "webGL/Build/webGL.wasm",
 
   });
 
 export class Game extends Component<{}, GameState>{
-    constructor(){
-        super({});
+    constructor(props: any){
+        super(props);
+        //super({});
         this.state = {
-            progress: 0
+            progress: 0,
+            coordinates: {x:0, y:0}
         };
 
-
+        this.clickClickFunc = this.clickClickFunc.bind(this);
     }
     componentDidMount(){
+        // EventListner на процесс загрузки
         unityContext.on("progress",  (progression) => {
             this.setState({
                 progress: progression
             });
           });
+
+        // EventListner на кастомный евент
+        unityContext.on("ClickEvent", (x, y, z) =>{
+            this.setState({
+                coordinates: {x: x, y: y}
+            })
+        });
     }
 
     clickClickFunc(){
-        unityContext.send("GameHandler", "TaskOnClick");
+        // Дергаю публичную функцию TaskOnClick из скрипта на объекте GameHandler
+        const objStr =  JSON.stringify(this.state.coordinates);
+        console.log(objStr)
+        unityContext.send("GameHandler", "TaskOnClick2", objStr);
     }
 
     render (){
@@ -51,6 +66,9 @@ export class Game extends Component<{}, GameState>{
                         width: 1110
                     }}/>
                 <button className="btn btn-primary" onClick={this.clickClickFunc}>ClickClick</button>
+                
+                <span>Координаты гексагона: {this.state.coordinates.x}, {this.state.coordinates.y}</span>
+                
             </div>
         )
     }
